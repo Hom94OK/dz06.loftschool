d914e36e3b332c2a08c0f3794b9280897964d76d
Hom94ok <khom.taras@gmail.com>
2017-08-07 20:44:26 +0300

    final commit

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,20 @@
-// POPUP
-$(".order__form-button").on("click", function (e) {
+$("#order__form-button").on("click", function (e) {
     e.preventDefault();
     // var dsa = 'tlkasn;lkdsa';
-    $(this).closest("body").find(".f-popup__bg").css({
+    $(this).closest("body").find("#f-order").css({
         'display': 'flex'
     });
-    // $( ".order__form-tag" ).load( "form-handler.php", function(data) {
-    //     $(this).closest("body").find(".f-popup__title").html(data);
-    // });
 
-    //$(this).closest('body').find('.f-popup__content').text(dsa);
     $.ajax({
         type: "POST",
         url: "form-handler.php",
         data: $(".order__form-tag").serialize() // serializes the form's elements.
     }).done(function (data) {
-        $(".f-popup__content").html(data);
-    })
+        $("#f-order").find(".f-popup__content").html(data);
+    });
 
-});
-$(".f-popup__close").on("click", function (e) {
-    e.preventDefault();
-    $(this).closest(".f-popup__bg").hide();
+    $("#f-order").find(".f-popup__close").on("click", function (e) {
+        e.preventDefault();
+        $(this).closest("#f-order").hide();
+    })
 });
